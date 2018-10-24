894a71934469716197335eaa8ee64ef555382911
udit01 <uditjainiitd@gmail.com>
2018-10-25 00:38:42 +0530

    Started py file

---
+++ BlackJackPlayer.py
@@ -0,0 +1,30 @@
+import sys
+
+class State:
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    prob = sys.argv[1]
+
+
+# Write to file in a specific format
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./BlackJackPlayer $1
+./BlackJackPlayer.py $1
