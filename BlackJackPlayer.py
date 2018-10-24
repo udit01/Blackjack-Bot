0eb162f9e4048e5a6312e92035b51988e769f74e
udit01 <uditjainiitd@gmail.com>
2018-10-25 01:14:01 +0530

    Added some state variables and diff action templates ?

--- BlackJackPlayer.py
+++ BlackJackPlayer.py
@@ -1,22 +1,26 @@
 import sys
 
 class State:
-    pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    def __init__(self):
+        self.has_pair = False
+        self.dealer_card = 2
+        self.total = 5
+        self.reward = 1.0 # To be calculated
+        self.has_ace = False
+        self.bet = 1.0
+    
+    def hit(self):
+        pass
+    
+    def stand(self):
+        pass
+    
+    def split(self):
+        pass
+    
+    def double(self):
+        pass
 
 
 
